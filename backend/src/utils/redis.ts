86a55c6890a87475dbe78c7742d68aed1d912fa8
behrad <behrad.beh83@Gmail.com>
2025-08-14 14:27:24 -0400

    redis file modified

--- backend/src/utils/redis.ts
+++ backend/src/utils/redis.ts
@@ -4,7 +4,9 @@ import logger from "../lib/logger";
 
 dotenv.config();
 
-const redis = new Redis(process.env.REDIS_URL!);
+const redis = new Redis(process.env.REDIS_URL!, {
+  tls: process.env.REDIS_URL?.startsWith("rediss://") ? {} : undefined,
+});
 
 redis.on("error", (err) => {
   logger.error("Redis error", err);
